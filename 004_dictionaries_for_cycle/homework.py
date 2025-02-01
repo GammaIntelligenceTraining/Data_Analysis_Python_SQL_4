ea5a5a25ea319ba92d5850b7453d9e600be9e9c8
Roman Kutselepa <roman.kutselepa@gmail.com>
2025-02-01 11:46:42 +0200

    latest

--- 004_dictionaries_for_cycle/homework.py
+++ 004_dictionaries_for_cycle/homework.py
@@ -8,16 +8,17 @@ set1 = {10, 20, 30, 40, 50}
 set2 = {30, 40, 50, 60, 70}
 
 
-# Delete a list of keys from a dictionary
-sample_dict = {
-    "name": "Kelly",
-    "age": 25,
-    "salary": 8000,
-    "city": "New York"
-}
+# Create two empty lists (long_names, short_names)
+# Iterate through names list and add names that are longer than 5 characters
+# to long_names, and others to short names
+names = ['Sarah', 'Jessica', 'Anthony', 'Jack', 'Simon', 'Arthur', 'Maria', 'Samantha']
 
 
-# You have a dictionary with keys 'names' and 'ages' and two empty lists as values.
-# Fill these lists with the elements from the set ('names' for strings, 'ages' for integers) 
-d = {'names': [], 'ages': []}
-s = {14, 27, 'Michel', 22, 'Helen', 'Adam', 48, 'Irma', 'Stefan', 19}
+# Given a list where each element is a year. Determine whether the given year is a leap year. If the year is a leap year,
+# print YES, otherwise print NO. According to the Gregorian calendar, a year is a leap year if its number is a multiple of 4,
+# but not a multiple of 100 OR if it is a multiple of 400.
+years_list = [2012, 2011, 1492, 1861, 1600, 1700, 1800, 1900, 2000]
+
+
+# Write a program that prompts the user for a string and checks if the string contains only unique characters.
+
