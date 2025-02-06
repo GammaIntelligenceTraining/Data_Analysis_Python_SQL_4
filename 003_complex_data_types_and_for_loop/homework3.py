7728368e716e6e08a9fbc87fe7cdddbc2f5d4d22
Roman Kutselepa <roman.kutselepa@gmail.com>
2025-02-06 12:14:42 +0200

    functions

--- 003_complex_data_types_and_for_loop/homework3.py
+++ 003_complex_data_types_and_for_loop/homework3.py
@@ -1,28 +1,74 @@
 # Print to console what is different in each set compared to another
 set_a = {1, 2, 3, 4}
 set_b = {3, 4, 5, 6}
-
+print(set_a.difference(set_b))
+print(set_b.difference(set_a))
+print(set_a - set_b)
 
 # Create a string from a list and save it to variable
 # Make each name on a new line.
 names = ['Jack', 'Mary', 'Samantha', 'George', 'Simon', 'John']
+names_string = "\n".join(names)
+print(names_string)
 
 
 # print sum of all numbers in a list
 # print sum of all unique numbers in a list
 numbers = [2, 53, 12, 87, 65, 32, 12, 2, 65, 32]
+# print(sum(numbers))
+# print(sum(set(numbers)))
 
+nums_sum = 0
+unique_sum = 0
+for num in numbers:
+    if type(num) is int or type(num) is float:
+        nums_sum += num
+
+# for num in set(numbers):
+#     unique_sum += num
+print(nums_sum)
+print(unique_sum)
 
 # create a new list from studentsA and studentsB
 # make sure there is no duplicates in a new lists
 studentsA = ['Jack', 'Bob', 'Mary']
 studentsB = ['Bob', 'Sarah', 'Simon']
+# new = list(set(studentsA + studentsB))
+# print(new)
+# new = []
+# for name in studentsA:
+#     if name not in new:
+#         new.append(name)
+# for name in studentsB:
+#     if name not in new:
+#         new.append(name)
+#
+# print(new)
+
 
 
 # What elements are common for both tuples?
 numbersA = (23, 52, 12, 75, 42)
 numbersB = (75, 22, 42, 94, 70)
-
+print(set(numbersA).intersection(set(numbersB)))
+print(set(numbersA) & set(numbersB))
 
 # add 5 to the tuple on a right position
 a = (1, 2, 3, 4, 6, 7, 8)
+# a = *a[:4], 5, *a[4:]
+# a = a[:4] + (5,) + a[4:]
+# a = (1, 2, 3, 4) + (5,) + (5, 6, 7)
+# a = list(a)
+# a.insert(4, 5)
+# a.append(5)
+# a.sort()
+# a = tuple(a)
+# print(a)
+
+# # add 5 after 31
+# b = (6, 4, 31, 76, 23, 13)
+# x = 5
+# b = list(b)
+# b.insert(b.index(31) + 1, x)
+# b = tuple(b)
+# print(b)
--- 004_dictionaries_for_cycle/homework.py
+++ 004_dictionaries_for_cycle/homework.py
@@ -1,24 +1,58 @@
 # Add a list of elements to a set
 sample_set = {"Yellow", "Orange", "Black"}
 sample_list = ["Blue", "Green", "Red"]
+# sample_set.update(sample_list)
+# for color in sample_list:
+#     sample_set.add(color)
+# sample_set |= set(sample_list)
+
+# print(sample_set)
 
 
 # Return a new set of identical items from two sets
 set1 = {10, 20, 30, 40, 50}
 set2 = {30, 40, 50, 60, 70}
+# set3 = set1.intersection(set2)
+# set3 = set1 & set2
+# print(set3)
 
 
 # Create two empty lists (long_names, short_names)
 # Iterate through names list and add names that are longer than 5 characters
 # to long_names, and others to short names
 names = ['Sarah', 'Jessica', 'Anthony', 'Jack', 'Simon', 'Arthur', 'Maria', 'Samantha']
+# long_names = []
+# short_names = []
+# for name in names:
+#     if len(name) > 5:
+#         long_names.append(name)
+#     else:
+#         short_names.append(name)
+# print(long_names)
+# print(short_names)
+
 
 
 # Given a list where each element is a year. Determine whether the given year is a leap year. If the year is a leap year,
 # print YES, otherwise print NO. According to the Gregorian calendar, a year is a leap year if its number is a multiple of 4,
 # but not a multiple of 100 OR if it is a multiple of 400.
 years_list = [2012, 2011, 1492, 1861, 1600, 1700, 1800, 1900, 2000]
-
+for y in years_list:
+    if y % 4 == 0 and y % 100 != 0 or y % 400 == 0:
+        print(y, 'YES')
+    else:
+        print(y, 'NO')
 
 # Write a program that prompts the user for a string and checks if the string contains only unique characters.
+# prompt = input('Enter something: ').replace(' ', '')
+# if len(prompt) == len(set(prompt)):
+#     print('All characters are unique.')
+# else:
+#     print('All characters are not unique.')
 
+# x = """Never odd or even"""
+# x_formated = x.lower().replace(' ', '')
+# x_reversed = x.lower().replace(" ", '')[::-1]
+#
+# if x_formated == x_reversed:
+#     print('Palindrome')
